67cd7fb09992dc011cfc6f75c30579c73e1dfdb3
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-04-26 15:10:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,28 @@ function App() {
   return (
     <Router>
       <div className="App" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        
+        {/* Red Stripe with Location and Phone Number */}
+        <div style={{ backgroundColor: "red", color: "white", padding: "0.5rem", textAlign: "center" }}>
+          <span>Location: 123 Main St | Phone: (123) 456-7890</span>
+        </div>
+
+        {/* Logo underneath the red stripe */}
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          <img 
+            src="https://i.imgur.com/JxNtFru.png" 
+            alt="Rafi Kitchen Logo" 
+            style={{ maxWidth: "200px", height: "auto" }} 
+          />
+        </div>
+
         {/* Navbar */}
-        <nav style={{ backgroundColor: "white", padding: "1rem", color: "purple", display: "flex", gap: "1rem" }}>
-          <Link to="/" style={{ color: "purple" }}>Home</Link>
-          <Link to="/menu" style={{ color: "purple" }}>Menu</Link>
-          <Link to="/about" style={{ color: "purple" }}>About</Link>
-          <Link to="/reservation" style={{ color: "purple" }}>Reservation</Link>
-          <Link to="/contact" style={{ color: "purple" }}>Contact</Link>
+        <nav style={{ backgroundColor: "white", padding: "1rem", color: "purple", display: "flex", gap: "1rem", justifyContent: "center" }}>
+          <Link to="/" style={{ color: "purple", textDecoration: "none" }}>Home</Link>
+          <Link to="/menu" style={{ color: "purple", textDecoration: "none" }}>Menu</Link>
+          <Link to="/about" style={{ color: "purple", textDecoration: "none" }}>About</Link>
+          <Link to="/reservation" style={{ color: "purple", textDecoration: "none" }}>Reservation</Link>
+          <Link to="/contact" style={{ color: "purple", textDecoration: "none" }}>Contact</Link>
         </nav>
 
         {/* Image Carousel */}
