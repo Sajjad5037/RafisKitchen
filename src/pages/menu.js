9808134b5d3ae59256c28896b557eb1d417c1d91
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-04-28 23:12:27 +0500

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,10 @@
 function Menu() {
   return (
     <div>
-      <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>Our Menu</h1>
+      <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
+      Our Menu (<span style={{ color: "red" }}>under construction</span>)
+    </h1>
+
       <p>Explore our delicious dishes 🍝🥗🍕</p>
     </div>
   );
