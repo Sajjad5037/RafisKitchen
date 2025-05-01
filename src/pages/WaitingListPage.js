eb2d6e78149519fef61e9b780e9f239aa633ef95
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-01 18:53:30 +0500

    Add files via upload

---
+++ src/pages/WaitingListPage.js
@@ -0,0 +1,17 @@
+// src/pages/WaitingListPage.jsx
+
+import React from "react";
+
+function WaitingListPage() {
+  return (
+    <iframe
+      src="https://clinic-management-system-27d11.web.app/dashboard?publicToken=c2c7afd8-c78a-4fc2-b6f4-1b92ffaaed4a&sessionToken=843081ac-bcab-4602-a2b6-6e28fd335331"
+      width="100%"
+      height="600px"
+      style={{ border: "none" }}
+      title="Waiting List"
+    />
+  );
+}
+
+export default WaitingListPage;
