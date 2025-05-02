bed661f4e937b26738286c8a492370408bc7314e
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-02 13:54:48 +0500

    Update WaitingListPage.js

--- src/pages/WaitingListPage.js
+++ src/pages/WaitingListPage.js
@@ -1,13 +1,16 @@
 // src/pages/WaitingListPage.jsx
 function WaitingListPage() {
   return (
-    <iframe
-      src="https://clinic-management-system-27d11.web.app/dashboard?publicToken=a7a429c9-e69c-46a0-a269-8a1e9634b882&sessionToken=fd92b5f1-a84f-466f-b72f-984819d9e458"
-      width="100%"
-      height="600px"
-      style={{ border: "none" }}
-      title="Waiting List"
-    />
+    <div>
+      <a
+        href="https://clinic-management-system-27d11.web.app/dashboard?publicToken=a7a429c9-e69c-46a0-a269-8a1e9634b882&sessionToken=fd92b5f1-a84f-466f-b72f-984819d9e458"
+        target="_blank" // Opens in a new tab
+        rel="noopener noreferrer" // Ensures security when opening in a new tab
+        style={{ textDecoration: "none", color: "blue", fontSize: "16px" }}
+      >
+        Go to the Waiting List Dashboard
+      </a>
+    </div>
   );
 }
 
