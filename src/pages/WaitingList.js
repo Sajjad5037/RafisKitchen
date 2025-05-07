2f4349019a5f0c32a07d0cf5b351b93c376fa74a
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-08 03:10:37 +0500

    Update WaitingList.js

--- src/pages/WaitingList.js
+++ src/pages/WaitingList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WaitingList = () => {
   return (
     <div style={{ padding: '1rem' }}>
-      <h2 style={{ color: 'purple', marginBottom: '1rem' }}>Clinic Management Dashboard</h2>
+      
       <iframe
         src="https://clinic-management-system-27d11.web.app/dashboard?publicToken=a7a429c9-e69c-46a0-a269-8a1e9634b882&sessionToken=fd92b5f1-a84f-466f-b72f-984819d9e458"
         title="Clinic Dashboard"
