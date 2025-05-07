0201d3e9e91d520fb59e68f3235a4667e0799ab6
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-08 01:15:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Reservation from "./pages/reservation";
 import Contact from "./pages/contact";
 import Chatbot from "./pages/Chatbot"; // Import Chatbot
 import OrderStatusPage from "./pages/OrderStatusPage";
-import Contact2 from "./pages/contactRafisKitchen";
+import ContactRK from "./pages/contactRafisKitchen";
 
 
 function App() {
@@ -61,7 +61,7 @@ function App() {
             <Route path="/reservation" element={<Reservation />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/order-status" element={<OrderStatusPage />} />
-            <Route path="/contact2" element={<Contact2 />} />
+            <Route path="/contact2" element={<ContactRK />} />
 
           </Routes>
         </div>
