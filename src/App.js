c162a6a14c1c1adeb06e9e53a7b38101a23f9547
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-05-08 03:20:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
           
           <Link to="/OrderOnline" style={{ color: "purple", textDecoration: "none" }}>Order Online</Link>
             <Link to="/reservation" style={{ color: "purple", textDecoration: "none" }}>Reservation</Link>
-            <Link to="/waiting-list" style={{ color: "purple", textDecoration: "none" }}>Waiting List</Link>
+            
           <Link to="/contact" style={{ color: "purple", textDecoration: "none" }}>
             Contact Us
           </Link>
@@ -63,7 +63,7 @@ function App() {
             <Route path="/reservation" element={<Reservation />} />
             <Route path="/OrderOnline" element={<OrderOnline />} />
             <Route path="/order-status" element={<OrderStatusPage />} />
-            <Route path="/waiting-list" element={<WaitingList />} />
+            
             <Route path="/contact" element={<Contact />} />
 
           </Routes>
